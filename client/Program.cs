21e27a8b8c1e2caa1dee90566fe4e526529dc068
Sakumoto Takuya <tak.sakumoto@gmail.com>
2023-09-13 18:41:54 +0900

    Add basic projects for client and server

---
+++ client/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace client
+{
+    static class Constants
+    {
+        public const int BUFFER_SIZE = 256;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get a server IP address
+            Console.Write("Input a server IP address > ");
+            string server_ip_address = Console.ReadLine();
+
+            // Get the server port number
+            Console.Write("Input the server port number > ");
+            string server_port_str = Console.ReadLine();
+            int server_port = Int32.Parse(server_port_str);
+
+            TcpClient tcp_client = null;
+
+            try
+            {
+                // Make a client for the server
+                tcp_client = new TcpClient(server_ip_address, server_port);
+
+                // Buffer
+                Byte[] bytes = new Byte[Constants.BUFFER_SIZE];
+                String recieved_data = null;
+
+                while (true)
+                {
+                    NetworkStream stream = tcp_client.GetStream();
+
+                    Console.Write("Type a message > ");
+                    string message = Console.ReadLine();
+
+                    // Get a now date time
+                    var now_date = DateTime.Now.ToString();
+
+                    // Send a messege
+                    var sent_data = Encoding.UTF8.GetBytes(message);
+                    stream.Write(sent_data, 0, sent_data.Length);
+                    Console.WriteLine("{0} Sent to the server: {1}", now_date, message);
+
+                    // Loop to receive data from the server
+                    int i;
+                    if ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Convert data to a string.
+                        recieved_data = Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+                        Console.WriteLine("Received from the server: {0}", recieved_data);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace server
+{
+    static class Constants
+    {
+        public const int BUFFER_SIZE = 256;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get a client IP address
+            Console.Write("Input a client IP address > ");
+            string client_ip_address_str = Console.ReadLine();
+            IPAddress client_ip_address = IPAddress.Parse(client_ip_address_str);
+
+            // Get the client port number
+            Console.Write("Input the client port number > ");
+            string client_port_str = Console.ReadLine();
+            int client_port = Int32.Parse(client_port_str);
+
+            TcpListener listener = null;
+
+            try
+            {
+                // Make a listener for the client
+                var ip_end_point = new IPEndPoint(client_ip_address, client_port);
+                listener = new TcpListener(ip_end_point);
+                listener.Start();
+
+                // Buffer for recieved data from the client
+                Byte[] bytes = new Byte[Constants.BUFFER_SIZE];
+                String recieved_data = null;
+
+                // Listening loop
+                while (true)
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+
+                    NetworkStream stream = client.GetStream();
+
+                    // Loop to receive data from the client
+                    int i;
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Print recieved data
+                        recieved_data = Encoding.UTF8.GetString(bytes, 0, i);
+                        Console.WriteLine("Received from the client: {0}", recieved_data);
+
+                        // Get a now date time
+                        var now_date = DateTime.Now.ToString();
+                        var now_date_bytes = Encoding.UTF8.GetBytes(now_date);
+
+                        // Send back a response
+                        stream.Write(now_date_bytes, 0, now_date_bytes.Length);
+                        Console.WriteLine(Encoding.UTF8.GetString(now_date_bytes));
+                        Console.WriteLine("Sent to the client: {0}", now_date);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                listener.Stop();
+            }
+        }
+    }
+}
