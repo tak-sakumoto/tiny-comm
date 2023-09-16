a8e21f5c6cd020407349b39bfdd7c05a1c2bad91
Sakumoto Takuya <tak.sakumoto@gmail.com>
2023-09-16 12:23:13 +0900

    Remove unused namespaces

--- server/server.cs
+++ server/server.cs
@@ -3,69 +3,66 @@ using System.Net.Sockets;
 using System.Net;
 using System.Text;
 
-namespace server
+internal class server
 {
-    internal class server
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        ConnectionInfo client_info = new ConnectionInfo();
+
+        // Get a client IP address
+        Console.Write("Input a client IP address > ");
+        client_info.ip_address_str = Console.ReadLine();
+
+        // Get the client port number
+        Console.Write("Input the client port number > ");
+        client_info.port = Int32.Parse(Console.ReadLine());
+
+        TcpListener listener = null;
+
+        try
         {
-            ConnectionInfo client_info = new ConnectionInfo();
+            // Make a listener for the client
+            client_info.set_ip_end_point();
+            listener = new TcpListener(client_info.ip_end_point);
+            listener.Start();
 
-            // Get a client IP address
-            Console.Write("Input a client IP address > ");
-            client_info.ip_address_str = Console.ReadLine();
+            // Buffer for recieved data from the client
+            Byte[] bytes = new Byte[Constants.BUFFER_SIZE];
+            String recieved_data = null;
 
-            // Get the client port number
-            Console.Write("Input the client port number > ");
-            client_info.port = Int32.Parse(Console.ReadLine());
-
-            TcpListener listener = null;
-
-            try
+            // Listening loop
+            while (true)
             {
-                // Make a listener for the client
-                client_info.set_ip_end_point();
-                listener = new TcpListener(client_info.ip_end_point);
-                listener.Start();
+                TcpClient client = listener.AcceptTcpClient();
 
-                // Buffer for recieved data from the client
-                Byte[] bytes = new Byte[Constants.BUFFER_SIZE];
-                String recieved_data = null;
+                NetworkStream stream = client.GetStream();
 
-                // Listening loop
-                while (true)
+                // Loop to receive data from the client
+                int i;
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
                 {
-                    TcpClient client = listener.AcceptTcpClient();
+                    // Print recieved data
+                    recieved_data = Encoding.UTF8.GetString(bytes, 0, i);
+                    Console.WriteLine("Received from the client: {0}", recieved_data);
 
-                    NetworkStream stream = client.GetStream();
+                    // Get a now date time
+                    var now_date = DateTime.Now.ToString();
+                    var now_date_bytes = Encoding.UTF8.GetBytes(now_date);
 
-                    // Loop to receive data from the client
-                    int i;
-                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
-                    {
-                        // Print recieved data
-                        recieved_data = Encoding.UTF8.GetString(bytes, 0, i);
-                        Console.WriteLine("Received from the client: {0}", recieved_data);
-
-                        // Get a now date time
-                        var now_date = DateTime.Now.ToString();
-                        var now_date_bytes = Encoding.UTF8.GetBytes(now_date);
-
-                        // Send back a response
-                        stream.Write(now_date_bytes, 0, now_date_bytes.Length);
-                        Console.WriteLine(Encoding.UTF8.GetString(now_date_bytes));
-                        Console.WriteLine("Sent to the client: {0}", now_date);
-                    }
+                    // Send back a response
+                    stream.Write(now_date_bytes, 0, now_date_bytes.Length);
+                    Console.WriteLine(Encoding.UTF8.GetString(now_date_bytes));
+                    Console.WriteLine("Sent to the client: {0}", now_date);
                 }
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-            finally
-            {
-                listener.Stop();
-            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+        finally
+        {
+            listener.Stop();
         }
     }
 }
